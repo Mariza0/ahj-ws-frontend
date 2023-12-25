c6f9d7add9edaecef23c9651ad840dff6e7bab44
Mariza0 <maria.net@mail.ru>
2023-12-26 06:55:09 +0700

    add js files

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,17 @@
 // TODO: write code here
 
 // comment this to pass build
-const unusedVariable = "variable";
+// const unusedVariable = "variable";
 
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
+// // for demonstration purpose only
+// export default function demo(value) {
+//   return `Demo: ${value}`;
+// }
 
-console.log("app.js included");
+// console.log("app.js included");
+import PlayGround from './playground/playground.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const goblin = new PlayGround(document.querySelector('.playground'));
+  setInterval(goblin.moveGoblin, 1000);
+});
