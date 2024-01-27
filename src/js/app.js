d60ee32f5f79fabc02a298129d1a1db0a088072d
Mariza0 <maria.net@mail.ru>
2024-01-28 00:26:21 +0100

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,63 @@
-// TODO: write code here
+const subscribeWidget = document.querySelector('.subscribe');
+const subscribeForm = subscribeWidget.querySelector('.subscribe-form');
+const nameInput = subscribeWidget.querySelector('.name');
+const phoneInput = subscribeWidget.querySelector('.phone');
+const unsubscribeBtn = subscribeWidget.querySelector('.unsubscribe-btn');
 
-// comment this to pass build
-// const unusedVariable = "variable";
+subscribeForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-// // for demonstration purpose only
-// export default function demo(value) {
-//   return `Demo: ${value}`;
-// }
+  const body = new FormData(subscribeForm);
 
-// console.log("app.js included");
-import PlayGround from './playground/playground.js';
+  const xhr = new XMLHttpRequest();
 
-document.addEventListener('DOMContentLoaded', () => {
-  const goblin = new PlayGround(document.querySelector('.playground'));
-  setInterval(goblin.moveGoblin, 1000);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) return;
+
+  }
+
+  xhr.open('POST', 'http://localhost:7070');
+  // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.send(body);
+});
+
+unsubscribeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const body = new FormData(subscribeForm);//Array.from(subscribeForm.elements)
+  //   .filter(({ name}) => name)
+  //   .map(({ name, value}) => `${name}=${encodeURIComponent(value)}`)
+  //   .join('&');
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) return;
+
+    console.log(xhr.responseText);
+  }
+
+  xhr.open('DELETE', 'http://localhost:7070/?' + body);
+  // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.send();
+});
+
+const uploadForm = document.querySelector('.upload-form');
+
+
+uploadForm.addEventListener('submit', (e) => {
+
+  e.preventDefault();
+
+  const body = new FormData(uploadForm);
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) return;
+  }
+
+  xhr.open('POST', 'http://localhost:7070/upload');
+  // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  xhr.send(body);
 });
